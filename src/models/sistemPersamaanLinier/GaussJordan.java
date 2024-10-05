634e0ed6a89944e2734c71aac549781a66d17ea0
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-10-05 18:25:08 +0700

    switch row & column index variable

--- src/models/sistemPersamaanLinier/GaussJordan.java
+++ src/models/sistemPersamaanLinier/GaussJordan.java
@@ -12,35 +12,35 @@ public class GaussJordan {
     private NormalizeMatrix normalizeMatrix = new NormalizeMatrix();
 
 
-    public Matrix gaussJordanElimination(Integer n, Integer m, Matrix matrix) {
+    public Matrix gaussJordanElimination(Integer m, Integer n, Matrix matrix) {
         AlignMatrix alignMatrix = new AlignMatrix();
         ForwardElimination forwardElimination = new ForwardElimination();
         BackwardElimination backwardElimination = new BackwardElimination();
         alignMatrix.alignMatrix(matrix);
-        int flag = checkConsistency.checkConsistency(n, m, matrix);
+        int flag = checkConsistency.checkConsistency(m, n, matrix);
         if(flag != -1) {
             Matrix singular = new Matrix(1, 1);
             singular.set(0, 0, (double)flag);
             return singular;
         }
 
-        forwardElimination.forwardElimination(n, m, matrix);
-        flag = checkConsistency.checkConsistency(n, m, matrix);
+        forwardElimination.forwardElimination(m, n, matrix);
+        flag = checkConsistency.checkConsistency(m, n, matrix);
         if(flag != -1) {
             Matrix singular = new Matrix(1, 1);
             singular.set(0, 0, (double)flag);
             return singular;
         }
 
-        backwardElimination.backwardElimination(n, m, matrix);
-        normalizeMatrix.normalizeMatrix(n, m, matrix);
+        backwardElimination.backwardElimination(m, n, matrix);
+        normalizeMatrix.normalizeMatrix(m, n, matrix);
         return matrix;
     }
 
     public Matrix main(Tuple3<Integer, Integer, Matrix> data) {
-        int n = data.getItem1();
-        int m = data.getItem2();
-        Matrix result = gaussJordanElimination(n, m, data.getItem3());
+        int m = data.getItem1();
+        int n = data.getItem2();
+        Matrix result = gaussJordanElimination(m, n, data.getItem3());
         return result;
     }
 }
