c65be8b0fac179a28f959c7ce64470a3f865a326
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-10-05 18:23:13 +0700

    fix: changing matrix index

--- src/helpers/GetConst.java
+++ src/helpers/GetConst.java
@@ -5,9 +5,10 @@ import src.datatypes.Matrix;
 public class GetConst {
     public Matrix getConst(Matrix arr) {
         int n = arr.getRowCount();
+        int m = arr.getColumnCount();
         Matrix result = new Matrix(n, 1);
         for (int i = 0; i < n; i++) {
-            result.set(i, 0, arr.get(i, n));
+            result.set(i, 0, arr.get(i, m-1));
         }
         return result;
     }
