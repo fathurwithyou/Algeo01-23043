f45ef24a98587b127e83f9b07429f9dcb919dcde
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-10-06 12:03:55 +0700

    fix: fix index range for case mxn

--- src/models/sistemPersamaanLinier/CheckConsistency.java
+++ src/models/sistemPersamaanLinier/CheckConsistency.java
@@ -5,21 +5,30 @@ import src.datatypes.Matrix;
 public class CheckConsistency {
     public int checkConsistency(Integer n, Integer m, Matrix matrix) {
         int flag = -1;
-        for(int i = 0; i < n; i++) {
+        // n = baris = persamaan
+        // m = kolom = peubah + 1
+        // we need peubah 
+        for(int i = 0; i < Math.min(n,m-1); i++) {
             boolean isZero = true;
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < Math.min(n,m-1); j++) {
                 if(matrix.get(i, j) != 0) {
                     isZero = false;
                     break;
                 }
             }
 
-            if(isZero && matrix.get(i, m-1) != 0) {
-                flag = 1;
+            if(isZero && matrix.get(i, Math.min(n,m-1)) != 0) {
+                flag = 1; //tidak memiliki solusi
                 break;
             }
-            if(isZero && matrix.get(i, m-1) == 0) {
-                flag = 0;
+
+            if(isZero && matrix.get(i, Math.min(n,m-1)) == 0) {
+                flag = 0; //memiliki banyak solusi
+            }
+
+            //case matriks persegi panjang dengan banyak peubah > banyak persamaan
+            if ((n < m-1)) {
+                flag = 0; //memiliki banyak solusi
             }
         }
         return flag;
