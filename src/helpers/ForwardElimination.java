ac843c8443344ddf5033d3fdf39e0fcda5e358bd
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-10-06 12:04:28 +0700

    fix: fix index range for case mxn

--- src/helpers/ForwardElimination.java
+++ src/helpers/ForwardElimination.java
@@ -3,8 +3,12 @@ package src.helpers;
 import src.datatypes.Matrix;
 
 public class ForwardElimination {
+
+    //temp
+    private static final double EPSILON = 1e-9;
+
     public int forwardElimination(Integer n, Integer m, Matrix matrix) {
-        for (int col = 0; col < n; col++) {
+        for (int col = 0; col < Math.min(m,n); col++) {
             for (int row = col + 1; row < n; row++) {      
                 if (matrix.get(col, col) == 0) {
                     return col;
@@ -12,6 +16,8 @@ public class ForwardElimination {
                 double multiplier = matrix.get(row, col) / matrix.get(col, col);
                 for (int i = col; i < matrix.getColumnCount(); i++) {
                     matrix.set(row, i, matrix.get(row, i) - multiplier * matrix.get(col, i));
+                    if (Math.abs(matrix.get(row, i)) < EPSILON)
+                        matrix.set(row, i, 0.0);
                 }
             }
         }
