9368b37ff6748f3ec57742732725607b3e39b54a
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-10-06 12:04:48 +0700

    fix: fix index range for case mxn

--- src/helpers/BackwardSubstitution.java
+++ src/helpers/BackwardSubstitution.java
@@ -4,15 +4,19 @@ import src.datatypes.Matrix;
 
 
 public class BackwardSubstitution {
+    private static final double EPSILON = 1e-9;
+    
     public Array backwardSubstitution(Matrix matrix) {
-        int n = matrix.getRowCount();
-        Array result = new Array(n);
-        for (int i = n - 1; i >= 0; i--) {
+        int n = matrix.getColumnCount();
+        Array result = new Array(n-1);
+        for (int i = n - 2; i >= 0; i--) {
             double sum = 0;
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n-1; j++) {
                 sum += matrix.get(i, j) * result.get(j);
             }
-            result.set(i, (matrix.get(i, n) - sum) / matrix.get(i, i));
+            result.set(i, (matrix.get(i, n-1) - sum) / matrix.get(i, i));
+            if (Math.abs(result.get(i)) < EPSILON)
+                    result.set(i, 0.0);
         }
         return result;
     }
