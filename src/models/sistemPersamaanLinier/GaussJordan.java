0b9575da318ea9e0ca4763f59650d337f82ce3b7
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-10-06 12:03:24 +0700

    fix: several method
    1. return result into array SPL solution
    2. convert result < 1e-9 into 0.0

--- src/models/sistemPersamaanLinier/GaussJordan.java
+++ src/models/sistemPersamaanLinier/GaussJordan.java
@@ -1,16 +1,22 @@
 package src.models.sistemPersamaanLinier;
 
+import src.datatypes.Array;
 import src.datatypes.Matrix;
 import src.datatypes.Tuple3;
-import src.helpers.ForwardElimination;
 import src.helpers.AlignMatrix;
 import src.helpers.BackwardElimination;
+import src.helpers.ForwardElimination;
 import src.helpers.NormalizeMatrix;
 
 public class GaussJordan {
     private CheckConsistency checkConsistency = new CheckConsistency();
     private NormalizeMatrix normalizeMatrix = new NormalizeMatrix();
 
+    //temp
+    private static final double EPSILON = 1e-9;
+
+        // //temp
+        // private SistemPersamaanLinierView view = new SistemPersamaanLinierView();
 
     public Matrix gaussJordanElimination(Integer m, Integer n, Matrix matrix) {
         AlignMatrix alignMatrix = new AlignMatrix();
@@ -24,6 +30,10 @@ public class GaussJordan {
             return singular;
         }
 
+        // System.out.println("Aligned Matrix:");
+        // view.printMatrix(matrix); //temp
+        // System.out.println();
+
         forwardElimination.forwardElimination(m, n, matrix);
         flag = checkConsistency.checkConsistency(m, n, matrix);
         if(flag != -1) {
@@ -32,15 +42,43 @@ public class GaussJordan {
             return singular;
         }
 
+        // System.out.println("For Eliminated Matrix:");
+        // view.printMatrix(matrix); //temp
+        // System.out.println();
+
         backwardElimination.backwardElimination(m, n, matrix);
+
+        // System.out.println("Back Eliminated Matrix:");
+        // view.printMatrix(matrix); //temp
+        // System.out.println();
+
         normalizeMatrix.normalizeMatrix(m, n, matrix);
+
+        // System.out.println("Normalized Matrix");
+        // view.printMatrix(matrix); //temp
+        // System.out.println();
+
         return matrix;
     }
 
-    public Matrix main(Tuple3<Integer, Integer, Matrix> data) {
+    public Array main(Tuple3<Integer, Integer, Matrix> data) {
         int m = data.getItem1();
         int n = data.getItem2();
-        Matrix result = gaussJordanElimination(m, n, data.getItem3());
-        return result;
+        Matrix rawResult = gaussJordanElimination(m, n, data.getItem3());
+
+        if (rawResult.getRowCount() == 1 && rawResult.getColumnCount() == 1) {
+            Array flag = new Array(1); 
+            flag.set(0, rawResult.get(0, 0));
+            return flag;
+
+        } else {
+            Array result = new Array(n-1);
+            for (int i = 0; i < n-1; i++) {
+                result.set(i, rawResult.get(i, n-1));
+                if (Math.abs(result.get(i)) < EPSILON)
+                    result.set(i, 0.0);
+            }
+            return result;
+        }   
     }
 }
