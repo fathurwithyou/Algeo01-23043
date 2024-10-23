b486d8199105435df1b4ea230ba78f7f225ffd88
adndax <adindaputri0517@gmail.com>
2024-10-23 09:24:56 +0700

    feat: img resizing controller

--- src/controllers/ImageResizingController.java
+++ src/controllers/ImageResizingController.java
@@ -1,5 +1,30 @@
 package src.controllers;
 
-public class ImageResizingControllers {
-    
+import src.views.imageResizing.ImageResizingViews;
+import src.models.imageResizing.ImageResizingModel;
+import java.awt.image.BufferedImage;
+
+public class ImageResizingController {
+
+    private ImageResizingViews view = new ImageResizingViews();
+    private ImageResizingModel model = new ImageResizingModel();
+
+    public void main() {
+        int choice = view.getChoice(); 
+
+        if (choice == 1) {
+            BufferedImage inputImage = view.getInputFromFile();
+
+            if (inputImage != null) {
+                view.showProcessingMessage();
+                int newWidth = view.getNewWidth();
+                int newHeight = view.getNewHeight();
+                BufferedImage processedImage = model.resizeImage(inputImage, newWidth, newHeight);
+                view.saveImageToFile(processedImage, "gambar_hasil");
+                view.showCompletionMessage();
+            }
+        } else {
+            System.out.println("Keluar");
+        }
+    }
 }
